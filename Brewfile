8e4def1fa67a4d99cdb79809744d3b1b16283362
Ondrej Slama <ondrej.slama@veeam.com>
2023-10-03 10:13:33 +0200

    Updates to brewfile and start_here (veeam) add karabiner config

--- Brewfile
+++ Brewfile
@@ -8,15 +8,10 @@ tap "homebrew/cask-versions"
 tap "heroku/brew"
 tap "teamookla/speedtest"
 
-# shell
-# ===
-
 brew "spaceship"
 
 # Essentials
 # ===
-brew "mas"
-
 brew "git-extras"
 brew "htop"
 brew "thefuck"
@@ -24,66 +19,21 @@ brew "wget"
 brew "tig"
 brew "hstr"
 brew "openssl"
-brew "openfortivpn"
-cask "macmediakeyforwarder"
-cask "scroll-reverser"
-cask "iterm2"
 
-brew "svn"
-brew "nvm"
-brew "redis"
 brew "python3"
-brew "python@3.10"
 brew "pylint"
 brew "pre-commit"
 brew "black"
 
-# plantuml
-# ===
-
-brew "openjdk"
-brew "graphviz"
-
-# Apps
-# ===
-
-# under license usually 
-# cask "docker"
-# alternative
-brew "podman"
-
-cask "ngrok"
-cask "1password"
-cask "slack"
+cask "docker"
+cask "karabiner-elements"
+cask "discord"
+cask "scroll-reverser"
+cask "raycast"
+cask "maccy"
+cask "iterm2"
 cask "signal"
-cask "transmission"
 cask "google-chrome"
-cask "firefox"
-cask "tor-browser"
 cask "visual-studio-code"
-mas "iMovie", id: 408981434
-mas "Xcode", id: 497799835
-cask "appcleaner"
 cask "vlc"
-mas "Amphetamine", id: 937984704
-mas "Magnet", id: 441258766
-cask "insomnia"
 cask "spotify"
-brew "handbrake"
-mas "Affinity Photo", id: 824183456
-
-# Fonts 
-# ===
-brew "svn" # requirement for fonts 
-cask "font-open-sans"
-cask "font-roboto"
-
-cask "font-hack"
-cask "font-roboto-mono"
-cask "font-source-code-pro"
-
-# Tools 
-# ===
-brew "exiftool"
-brew "speedtest"
-cask "android-file-transfer"
--- start_here.sh
+++ start_here.sh
@@ -20,11 +20,17 @@ nvm current
 npm i -g spaceship-prompt
 
 # For docker
-brew install colima
-brew install docker docker-compose
+# brew install colima
+# brew install docker docker-compose
 mkdir -p ~/.docker/cli-plugins
 ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
 
+# Setup git identity
+git config --global user.name "Ondrej Slama"
+git config --global user.email "ondrej.slama@veeam.com"
+
+# Setup vs-code as git editor (commit messages, rebase, etc.)
+git config --global core.editor "code --wait"
 
 # Close any open System Preferences panes, to prevent them from overriding settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
@@ -103,7 +109,10 @@ killall SystemUIServer
 # Set the icon size of Dock items to 45 pixels
 defaults write com.apple.dock tilesize -int 45
 
-#Autohide Dock
+# Set the dock orientation to left
+defaults write com.apple.dock orientation -string left
+
+# Autohide Dock
 defaults write com.apple.dock autohide -boolean true
 
 # Minimalize effect scale
@@ -135,41 +144,18 @@ done
 killall Dock
 
 # set DNS servers
-networksetup -setdnsservers Wi-Fi 8.8.8.8 1.1.1.1
+# networksetup -setdnsservers Wi-Fi 8.8.8.8 1.1.1.1
 
 # install pip tools
 pip3 install pip-tools
 
 # New machine - new ssh key
-eval "$(ssh-agent -s)"
+# eval "$(ssh-agent -s)"
 
-ssh-keygen -t rsa -b 4096 -C "ondrej.slama@rossum.ai"
+# ssh-keygen -t rsa -b 4096 -C "ondrej.slama@veeam.com"
 
-ssh-add -K ~/.ssh/id_rsa
+# ssh-add -K ~/.ssh/id_rsa
 
-# GUI Applications
-brew install --cask \
-  google-chrome  \
-  iterm2 \
-  visual-studio-code \
-  docker \
-  rectangle \
-  slack \
-  discord \
-  vlc \
-  maccy \
-  unnaturalscrollwheels \
-  raycast
-
-# Terminal Applications
-brew install \
-  wget \
-  exa \
-  git \
-  graphicsmagick \
-  commitzen \
-  cmatrix \
-  fig
 
 # activate shell
 source "$HOME/.zshrc"
